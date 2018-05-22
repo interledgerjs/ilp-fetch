f27555526261332718f03e3aff750c1aacc73b24
sharafian <ben@sharafian.me>
2018-05-21 17:03:06 -0700

    feat: add stream method

---
+++ src/stream.js
@@ -0,0 +1,29 @@
+const { createConnection } = require('ilp-protocol-stream')
+const debug = require('debug')('ilp-fetch:stream')
+const fetch = require('node-fetch')
+
+async function streamPayment ({
+  url,
+  opts,
+  payParams,
+  plugin,
+  payToken
+}) {
+  const [ destinationAccount, _sharedSecret ] = payParams
+  debug('streaming via STREAM. destination=' + destinationAccount)
+
+  const sharedSecret = Buffer.from(_sharedSecret, 'base64')
+  const connection = await createConnection({
+    plugin,
+    destinationAccount,
+    sharedSecret
+  })  
+
+  const stream = connection.createStream()
+  stream.setSendMax(opts.maxPrice)
+
+  await new Promise(resolve => setTimeout(resolve, 100))
+  return fetch(url, opts)
+}
+
+module.exports = streamPayment
